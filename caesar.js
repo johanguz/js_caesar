af849844bcd6e25655639d2050da025c35717c6f
Johan.Guzman <johanguz@gmail.com>
2021-02-05 15:12:21 -0500

    pass tests version

--- caesar.js
+++ caesar.js
@@ -5,6 +5,11 @@ const caesar = function(str, shift = 0) {
   let inputStr = str.split("");
   let finishedArr = [];
 
+  if (Math.sign(shift) === -1 ) {
+    shift = shift + alphabetArr.length;
+    console.log(shift)
+  }
+
   for (let str of inputStr) {
     if (str == str.toUpperCase()) {
       let idx = alphabetUpperArr.indexOf(str);
@@ -12,7 +17,7 @@ const caesar = function(str, shift = 0) {
         finishedArr.push(str);
       }
         else {
-        finishedArr.push((alphabetUpperArr[(idx + shift)]))
+        finishedArr.push((alphabetUpperArr[(idx + shift) % alphabetUpperArr.length]))
     }
   }
     else {
@@ -21,7 +26,7 @@ const caesar = function(str, shift = 0) {
       finishedArr.push(str);
     }
     else {
-    finishedArr.push((alphabetArr[(idx + shift)]))
+    finishedArr.push((alphabetArr[(idx + shift) % alphabetArr.length ]))
     }
   }
 }
